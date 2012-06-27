3aef9d404b3e04b034c8d2aa621c7d32471ade55
Nikunj Mehta <paradox924x@gmail.com>
2012-06-26 22:27:23 -0400

    Created auth system that interacts with nickserv (User must be registered on server). Added new config files users.cfg and commands.cfg for management of permissions. Reorganized several pieces of code.

--- bot.py
+++ bot.py
@@ -12,6 +12,12 @@ import sys
 
 ####
 
+config = RawConfigParser()
+user_config = RawConfigParser()
+command_config = RawConfigParser()
+
+####
+
 def get_config(key, cfg=config):
     return cfg.get('enigma-irc-bot', key)
 
@@ -68,6 +74,8 @@ def extract_command(line):
     message_parts = line.split(' ')
     if len(message_parts) > 3 and len(message_parts[3]) > 2 and message_parts[3][1] == get_config('command_prefix'):
         command = message_parts[3][2:]
+        if command not in commands:
+            command = None
     return command
 
 ####
@@ -95,6 +103,38 @@ def user_join_channel(channel):
     write('JOIN ' + channel)
     return
 
+def user_request_auth(user):
+    if user not in authed_users:
+        send_privmsg(get_config('nickserv'), 'STATUS ' + user)
+        return True
+    return False
+
+def user_auth(user):
+    if user not in authed_users:
+        authed_users.append(user)
+        authed_users.sort()
+    return
+
+def user_deauth(user):
+    if user not in authed_users:
+        return False
+    authed_users.remove(user)
+    return True
+
+def user_check_auth(user, command):
+    if command not in commands:
+        return False
+    authed_groups = command_groups_list[commands.index(command)]
+    if '*' in authed_groups:
+        return True
+    if user not in users or user not in authed_users:
+        return False
+    for authed_group_index, authed_group in enumerate(authed_groups):
+        authed_group_users = group_users_list[groups.index(authed_group)]
+        if user in authed_group_users:
+            return True
+    return False
+
 ####
 
 def print_out(line):
@@ -113,6 +153,8 @@ def write(line, is_silent=False):
 ####
 
 def signal_handler(signal, frame):
+    command_config_fp.close()
+    user_config_fp.close()
     config_fp.close()
     if s:
         write('QUIT ' + VERSION)
@@ -122,14 +164,48 @@ def signal_handler(signal, frame):
 
 #### Start
 
-VERSION  = 'EnigmaIRCb v0.1beta'
-GREETING = 'Welcome to ' + VERSION + '!'
+VERSION          = 'EnigmaIRCb v0.2beta'
+GREETING         = 'Welcome to ' + VERSION + '!'
+CONFIG_FILE_NAME = 'bot.cfg'
 
 signal.signal(signal.SIGINT, signal_handler)
 
-config = RawConfigParser()
-config_fp = open('bot.cfg')
+config_fp = open(CONFIG_FILE_NAME)
 config.readfp(config_fp)
+
+user_config_fp = open(get_config('user_config'))
+user_config.readfp(user_config_fp)
+
+command_config_fp = open(get_config('command_config'))
+command_config.readfp(command_config_fp)
+
+users = []
+authed_users = []
+
+groups, group_users_list = map(list, zip(*user_config.items('enigma-irc-bot')))
+for group_users_index, group_users in enumerate(group_users_list):
+    group_users = group_users.split(',')
+    for group_user_index, group_user in enumerate(group_users):
+        group_user = group_user.strip()
+        group_users[group_user_index] = group_user
+        if group_user not in users:
+            users.append(group_user)
+    group_users_list[group_users_index] = group_users
+users.sort()
+
+commands, command_groups_list = map(list, zip(*command_config.items('enigma-irc-bot')))
+for command_groups_index, command_groups in enumerate(command_groups_list):
+    command_groups = command_groups.split(',')
+    for command_group_index, command_group in enumerate(command_groups):
+        command_group = command_group.strip()
+        command_groups[command_group_index] = command_group
+        if command_group not in groups and command_group is not '?' and command_group is not '*':
+            print_out("FATAL ERROR: Unrecognized group '" + command_group + "' specified for command '" + commands[command_groups_list.index(command_groups)] + "'")
+            command_config.close()
+            user_config.close()
+            config.close()
+            sys.exit(1)
+    command_groups_list[command_groups_index] = command_groups
 
 print_out('='*len(GREETING))
 print_out(GREETING)
@@ -178,6 +254,15 @@ while line:
         for channel, users in channel_users.iteritems():
             if nick in users:
                 channel_users[channel].remove(nick)
+#### NickServ Messages
+    elif extract_nick(line) == get_config('nickserv'):
+        if message_parts[1] == 'NOTICE' and message_parts[3][1:] == 'STATUS':
+            nick = message_parts[4]
+            if message_parts[5] == '3':
+                user_auth(nick)
+                send_notice(nick, 'You have been successfully authenticated.')
+            else:
+                send_notice(nick, 'Failed to authenticate with ' + get_config('nickserv') + '.')
 #### Server Messages
     elif extract_reply_code(line):
         if reply_code == 1:
@@ -194,8 +279,21 @@ while line:
                     channel_users[channel].append(nick)
             channel_users[channel].sort()
 #### User Commands
-    elif extract_command(line):
-        if command == 'version':
+    elif extract_command(line) and user_check_auth(extract_nick(line), command):
+        if command == 'auth':
+            if not user_request_auth(extract_nick(line)):
+                send_notice(nick, 'You are already authenticated.')
+        elif command == 'commands':
+            commands_sorted = commands
+            commands_sorted.sort()
+            send_privmsg(extract_target(line), 'Commands: ' + ', '.join(commands_sorted))
+        elif command == 'deauth':
+            nick = extract_nick(line)
+            if user_deauth(nick):
+                send_notice(nick, 'You have been successfully deauthenticated.')
+            else:
+                send_notice(nick, 'You are not authenticated.')
+        elif command == 'version':
             send_privmsg(extract_target(line), 'Version: ' + VERSION)
 
     line = f.readline().rstrip()
